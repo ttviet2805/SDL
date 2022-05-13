d94dcea823c1ec2c995469c2e954a9b492f62f07
ttviet <ttviet2805@gmail.com>
2022-05-13 22:23:25 +0700

    Update

--- systemProcess.cpp
+++ systemProcess.cpp
@@ -1,5 +1,23 @@
 #include "systemProcess.h"
 
 void systemProcess() {
-    loginWindow();
+//    Account* curAccount = loginWindow();
+//
+//    if(curAccount == nullptr) {
+//        return;
+//    }
+
+//    if(curAccount->type == 0) {
+//
+//    }
+//    else {
+        Student* allStudent = nullptr;
+        loadAllStudentData(allStudent, studentFileName);
+
+//        Student* curStudent = findStudentByID(allStudent, curAccount->username);
+
+        studentWindow(allStudent);
+
+//        studentEditProfileWindow(allStudent);
+//    }
 }
