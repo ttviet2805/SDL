d3f5069691422909cd648eb4b7d9a76323558604
Hoang Nghia Viet <viethoangnghia@gmail.com>
2022-05-13 09:58:27 +0700

    Update

--- systemProcess.cpp
+++ systemProcess.cpp
@@ -1,31 +1,17 @@
 #include "systemProcess.h"
 
 void systemProcess() {
-    SDL_Window* window = NULL;
-    SDL_Surface* screenSurface = NULL;
-    SDL_Renderer* renderer = NULL;
-    SDL_Texture* texture = NULL;
+    SDL_Window* gWindow = NULL;
+    SDL_Renderer* gRenderer = NULL;
     SDL_Event event;
 
-    if(!init(window, renderer, "Test")) {
+    if(!init(gWindow, gRenderer, "Test")) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
     }
     else {
-        //Get window surface
-        screenSurface = SDL_GetWindowSurface(window);
+        SDL_RenderClear(gRenderer);
 
-        //Fill the surface white
-        SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-        //Update the surface
-        SDL_UpdateWindowSurface(window);
-
-        SDL_RenderClear(renderer);
-
-        Button TestButton = {SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2 - 20, 100, 40, 4, 4, BLACK, LIGHTBLUE, RED, BLACK, ""};
-        TestButton.Display(renderer);
-
-        SDL_RenderPresent(renderer);
+        Button TestButton = {300, 200, 100, 700, 4, BLACK, LIGHTBLUE, RED, BLACK, "a a a a a a a a a a aass a asss as as", 28};
 
         bool quit = false;
         while(!quit) {
@@ -34,28 +20,14 @@ void systemProcess() {
                     quit = true;
                     break;
                 }
-                int MouseType = isMouseClick(&event, &TestButton.bRect);
-                TestButton.FillCol = TestButton.InitCol;
-                if(MouseType == 1) {
-                    printf("left\n");
-                    TestButton.FillCol = TestButton.PressCol;
-                }
-                else if(MouseType == 2) {
-                    printf("hover\n");
-                    TestButton.FillCol = TestButton.HoverCol;
-                }
-                TestButton.Display(renderer);
 
-                SDL_RenderPresent(renderer);
+//                TestButton.TypeBox = 1;
+//                TestButton.isTextBox(gRenderer, &event);
+
+                TestButton.Display(gRenderer);
+
+                SDL_RenderPresent(gRenderer);
             }
         }
     }
-
-    SDL_FreeSurface(screenSurface);
-
-    //Destroy window
-    SDL_DestroyWindow(window);
-
-    //Quit SDL subsystems
-    SDL_Quit();
 }
