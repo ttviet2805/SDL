9b6b1ea1acdccbf9c7221381fa44694144ea8568
ttviet <ttviet2805@gmail.com>
2022-05-13 19:30:17 +0700

    Update

--- Const.h
+++ Const.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <SDL.h>
 using namespace std;
 
 //Screen dimension constants
@@ -14,4 +15,16 @@ const SDL_Color LIGHTBLUE = {0, 255, 255, 255};
 const SDL_Color VIOLET = {143, 80, 157, 255};
 const SDL_Color GREY = {216, 216, 216, 255};
 
+//Font constants
 const string FONTDIR = "Data/Font/times-new-roman-14.ttf";
+
+// File constants
+const string accountFileName = "Data/AllAccount.txt";
+const string studentFileName = "Data/AllStudent.txt";
+const string courseFileName = "Data/AllCourse.txt";
+const string courseScoreFileName = "Data/CourseScore/";
+const string classFileName = "Data/AllClass.txt";
+const string studentScoreFileName = "Data/StudentScore/";
+const string staffFileName = "Data/AllStaff.txt";
+const string studentInClassFileName = "Data/Class/";
+const string studentCSVFileName = "Data/StudentCSV.csv";
